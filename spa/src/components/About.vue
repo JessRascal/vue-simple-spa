bd3c7cf5bb07dbc35fe89dae5fa8759d94b49200
Jess Rascal <me@jessrascal.com>
2018-09-10 15:49:29 +0100

    Initial commit

---
+++ spa/src/components/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="about">
+    These are the details about this thing.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about'
+}
+</script>
+
+<style>
+#about {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
